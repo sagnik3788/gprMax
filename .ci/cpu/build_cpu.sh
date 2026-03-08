eb6e90da97de06f53e7d8e96623844f09f38b3f2
Sagnik Das <sagnikdas5432@gmail.com>
2026-03-08 23:56:05 +0530

    fix file lock error in case of mpi

--- .ci/cpu/build_cpu.sh
+++ .ci/cpu/build_cpu.sh
@@ -20,4 +20,6 @@ python3 -c "from gprMax.cython.fields_updates_normal import update_magnetic"
 python3 -c "from gprMax.cython.pml_build import pml_average_er_mr"
 
 # Verify (MPI)
+# to avoid file lock
+export HDF5_USE_FILE_LOCKING=FALSE
 mpirun -n 2 python3 -m gprMax examples/cylinder_Ascan_2D.in
